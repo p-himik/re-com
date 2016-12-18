97a9fd9b2ec596a0dc6ce01c53fae0f62885ce0a
p-himik <p1himik@gmail.com>
2016-12-18 22:08:36 +0700

    Make typeahead suggestions container on top of other elements

--- src/re_com/typeahead.cljs
+++ src/re_com/typeahead.cljs
@@ -2,7 +2,7 @@
   (:require-macros [re-com.core :refer [handler-fn]]
                    [cljs.core.async.macros :refer [alt! go-loop]])
   (:require [cljs.core.async :refer [chan timeout <! put!]]
-            [re-com.misc     :refer [throbber input-text]]
+            [re-com.misc     :refer [throbber input-text input-text-args-desc]]
             [re-com.util     :refer [deref-or-value px]]
             [re-com.popover  :refer [popover-tooltip]] ;; need?
             [re-com.box      :refer [h-box v-box box gap line flex-child-style align-style]] ;; need?
@@ -17,26 +17,16 @@
 ;; ------------------------------------------------------------------------------------
 
 (def typeahead-args-desc
-  [{:name :data-source       :required true                   :type "fn"               :validate-fn fn?                :description [:span [:code ":data-source"] " supplies suggestion objects. This can either accept a single string argument (the search term), or a string and a callback. For the first case, the fn should return a collection of suggestion objects (which can be anything). For the second case, the fn should return "[:code "nil" ]", and eventually result in a call to the callback with a collection of suggestion objects."]}
+  (conj input-text-args-desc
+   {:name :data-source       :required true                   :type "fn"               :validate-fn fn?                :description [:span [:code ":data-source"] " supplies suggestion objects. This can either accept a single string argument (the search term), or a string and a callback. For the first case, the fn should return a collection of suggestion objects (which can be anything). For the second case, the fn should return "[:code "nil" ]", and eventually result in a call to the callback with a collection of suggestion objects."]}
    {:name :on-change         :required false :default nil     :type "string -> nil"    :validate-fn fn?                :description [:span [:code ":change-on-blur?"] " controls when it is called. It is passed a suggestion object."] }
    {:name :change-on-blur?   :required false :default true    :type "boolean | atom"                                   :description [:span "when true, invoke " [:code ":on-change"] " when the use chooses a suggestion, otherwise invoke it on every change (navigating through suggestions with the mouse or keyboard, or if "[:code "rigid?"]" is also "[:code "false" ]", invoke it on every character typed.)"] }
-   {:name :model             :required false :default nil     :type "object | atom"                                             :description "The initial value of the typeahead (should match the suggestion objects returned by " [:code ":data-source"] ")."}
+   {:name :model             :required false :default nil     :type "object | atom"                                    :description "The initial value of the typeahead (should match the suggestion objects returned by " [:code ":data-source"] ")."}
    {:name :debounce-delay    :required false :default 250     :type "integer"          :validate-fn integer?           :description [:span "After receiving input, the typeahead will wait this many milliseconds without receiving new input before calling " [:code ":data-source"] "."]}
    {:name :render-suggestion :required false                  :type "render fn"        :validate-fn fn?                :description "override the rendering of the suggestion items by passing a fn that returns hiccup forms. The fn will receive two arguments: the search term, and the suggestion object."}
    {:name :suggestion-to-string :required false               :type "suggestion -> string" :validate-fn fn?            :description "When a suggestion is chosen, the input-text value will be set to the result of calling this fn with the suggestion object."}
-   {:name :rigid?            :required false :default true    :type "boolean | atom"                                   :description [:span "If "[:code "false"]" the user will be allowed to choose arbitrary text input rather than a suggestion from " [:code ":data-source"]". In this case, a string will be supplied in lieu of a suggestion object." ]}
+   {:name :rigid?            :required false :default true    :type "boolean | atom"                                   :description [:span "If "[:code "false"]" the user will be allowed to choose arbitrary text input rather than a suggestion from " [:code ":data-source"]". In this case, a string will be supplied in lieu of a suggestion object." ]}))
 
-   ;; the rest of the arguments are forwarded to the wrapped `input-text`
-   {:name :status            :required false                  :type "keyword"          :validate-fn input-status-type? :description [:span "validation status. " [:code "nil/omitted"] " for normal status or one of: " input-status-types-list]}
-   {:name :status-icon?      :required false :default false   :type "boolean"                                          :description [:span "when true, display an icon to match " [:code ":status"] " (no icon for nil)"]}
-   {:name :status-tooltip    :required false                  :type "string"           :validate-fn string?            :description "displayed in status icon's tooltip"}
-   {:name :placeholder       :required false                  :type "string"           :validate-fn string?            :description "background text shown when empty"}
-   {:name :width             :required false :default "250px" :type "string"           :validate-fn string?            :description "standard CSS width setting for this input"}
-   {:name :height            :required false                  :type "string"           :validate-fn string?            :description "standard CSS height setting for this input"}
-   {:name :disabled?         :required false :default false   :type "boolean | atom"                                   :description "if true, the user can't interact (input anything)"}
-   {:name :class             :required false                  :type "string"           :validate-fn string?            :description "CSS class names, space separated"}
-   {:name :style             :required false                  :type "CSS style map"    :validate-fn css-style?         :description "CSS styles to add or override"}
-   {:name :attr              :required false                  :type "HTML attr map"    :validate-fn html-attr?         :description [:span "HTML attributes, like " [:code ":on-mouse-move"] [:br] "No " [:code ":class"] " or " [:code ":style"] "allowed"]}])
 
 ;; TODO
 ;; ability to focus & blur the input-text would be nice... this is also missing from input-text
@@ -252,9 +242,7 @@
     (search-data-source-loop! state-atom c-search)
     (fn
       [& {:as   args
-          :keys [data-source rigid? render-suggestion suggestion-to-string model
-                 ;; forwarded to wrapped `input-text`:
-                 placeholder width height status-icon? status status-tooltip disabled? class style]}]
+          :keys [data-source rigid? render-suggestion suggestion-to-string model width]}]
       {:pre [(validate-args-macro typeahead-args-desc args "typeahead")]}
       (let [{:as state :keys [suggestions waiting? suggestion-active-index external-model]} @state-atom
             last-data-source (:data-source state)
@@ -266,21 +254,16 @@
           (swap! state-atom external-model-changed latest-external-model))
         [v-box
          :width width
+         :style {:display :inline}
          :children
-         [[input-text
-           :model          input-text-model
-           :class          class
-           :style          style
-           :disabled?      disabled?
-           :status-icon?   status-icon?
-           :status         status
-           :status-tooltip status-tooltip
-           :width          width
-           :height         height
-           :placeholder    placeholder
-           :on-change      (partial input-text-on-change! state-atom)
-           :change-on-blur? false
-           :attr {:on-key-down (partial input-text-on-key-down! state-atom)}]
+         [(into [input-text
+                 :model input-text-model
+                 :on-change (partial input-text-on-change! state-atom)
+                 ;:change-on-blur? false
+                 :attr (merge {:on-key-down (partial input-text-on-key-down! state-atom)}
+                              (:attr args))]
+                (apply concat (dissoc (select-keys args (map :name input-text-args-desc))
+                                      :model :on-change :attr)))
           (if (or (not-empty suggestions) waiting?)
             [v-box
              :class "rc-typeahead-suggestions-container"
